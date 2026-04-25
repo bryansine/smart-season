8bb8e88e574613edf1cd1394af036b40f33238a7
bryansine <bryansine1738@gmail.com>
2026-04-25 19:16:41 +0300

    refactoring code

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth.models import User; u = User.objects.get(username='admin_user'); u.set_password('password123'); u.save()"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); u = User.objects.get(username='admin_user'); u.set_password('password123'); u.save()"
